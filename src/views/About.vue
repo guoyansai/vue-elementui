a3b64147d5a3351b050308e9390edcb963184843
guoyansai <guoyansai@qq.com>
2021-12-14 19:28:33 +0800

    更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,85 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+    <div class="demo-input-suffix">
+      属性方式：
+      <el-input
+        placeholder="请选择日期"
+        suffix-icon="el-icon-date"
+        v-model="input1"
+      >
+      </el-input>
+      <el-input
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        v-model="input2"
+      >
+      </el-input>
+    </div>
+    <div class="demo-input-suffix">
+      slot 方式：
+      <el-input placeholder="请选择日期" v-model="input3">
+        <i slot="suffix" class="el-input__icon el-icon-date"></i>
+      </el-input>
+      <el-input placeholder="请输入内容" v-model="input4">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+    </div>
+    <div class="area">
+      哈哈
+      <div class="love">haha</div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value: "",
+      input1: "",
+      input2: "",
+      input3: "",
+      input4: "",
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+.area {
+  border: 1px solid #000;
+  .love {
+    color: aqua;
+  }
+}
+</style>
